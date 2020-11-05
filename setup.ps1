a2ee7b1ad5172d66dc0405d08be0a0c0ce3d5cd8
alec-hs <6503411+alec-hs@users.noreply.github.com>
2020-11-05 10:38:33 +0000

    Added PS as admin and more start menu stuff

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,5 @@
+# Currently tested and working on WIN10 x64 v20H2
+
 # Set execution policy to allow online PS scripts for this session
 Set-ExecutionPolicy -ExecutionPolicy 'RemoteSigned' -Scope 'Process' -Force
 
@@ -18,14 +20,22 @@ do {
     $updates = Get-WindowsUpdate
 } until ($updates.count -eq 0)
 
-#Set Start Menu Layout
+# Add open PS as admin to Shift + Right Click Menu
+reg import './reg-files/Open-PS-As-Admin-Shift-Right-Click.reg'
+
+# Set Start Menu Layout to Empty
 Copy-Item './LayoutModification.xml' C:\Users\$env:UserName\AppData\Local\Microsoft\Windows\Shell -Force
 Remove-Item -Force -Recurse -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\CloudStore\Store'
 
 # Set File Explorer Options
 $key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
-Set-ItemProperty $key 'Hidden' 1
-Set-ItemProperty $key 'HideFileExt' 0
-Set-ItemProperty $key 'ShowCortanaButton' 0
-Set-ItemProperty $key 'LaunchTo' 1
+Set-ItemProperty $key 'Hidden' 1                    # Show hidden file
+Set-ItemProperty $key 'HideFileExt' 0               # Show file extensions
+Set-ItemProperty $key 'ShowCortanaButton' 0         # Hide Cortana button on taskbar
+Set-ItemProperty $key 'LaunchTo' 1                  # Launch Explorer to "This PC"
+Set-ItemProperty $key 'AutoCheckSelect' 1           # Show check boxes in explorer
 
+$key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search'
+Set-ItemProperty $key 'SearchbocxTaskbarMode' 0     # Hide search box on taskbar
+
+Stop-Process -processname explorer
