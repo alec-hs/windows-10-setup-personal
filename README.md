c2ac6710536b05d73b1b9543dac464857c3f9126
alec-hs <6503411+alec-hs@users.noreply.github.com>
2020-11-08 14:30:21 +0000

    Add PS profile check

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Windows-10-Setup-Personal
 Scripts to setup Windows 10 PCs.
+
+
+How to use:
+
+    1 - Install Windows and Setup Account
+    2 - Login
+    3 - Open Edge and download this repo
+    4 - Run PS as Admin
+    5 - Execute 
+        `set-executionpolicy remotesigned`
+    6 - Run the script
+        `./setup.ps1`
--- setup.ps1
+++ setup.ps1
@@ -2,13 +2,22 @@
 # Need to open PS as admin and Set-Location to script folder
 # Reg import steps will through error if run in PS ISE
 
+# Check if PS Profile exists and create one if not
+Write-Host 'Checking for or creating PS Profile...' `n
+if (!(Test-Path -Path $Profile)) {
+    New-Item -ItemType File -Path $Profile -Force
+}
+
+# Reload profile
+.$Profile
+
 # Set execution policy to allow online PS scripts for this session
 Write-Host 'Setting Execution Policy for Session...' `n
 Set-ExecutionPolicy -ExecutionPolicy 'RemoteSigned' -Scope 'Process' -Force
 
 # Install NuGet Package Manager
 Write-Host 'Installing NuGet Package Manager...' `n
-Install-PackageProvider -Name 'NuGet' -Force
+Install-PackageProvider -Name 'NuGet' -Force | Out-Null
 
 # Set MS Powershell Gallery to trusted source
 Write-Host 'Trusting MS Powershell Gallery...' `n
@@ -21,17 +30,18 @@ Import-Module 'PSWindowsUpdate'
 
 # Get updates and install
 do {
+    Write-Host 'If updates are not run most app instals will be skipped.'
     $updateChoice = Read-Host -Prompt 'Would you like to run updates now? (y|n)'
 } until ($updateChoice -eq 'y' -Or $updateChoice -eq 'n')
 
-if $updateChoice -eq 'y' {
+if ($updateChoice -eq 'y') {
+    Write-Host 'Installing Windows Updates...' `n
     $updates = Get-WindowsUpdate
     do {
-        Install-WindowsUpdate -AcceptAll -IgnoreReboot
+        Install-WindowsUpdate -AcceptAll -IgnoreReboot | Out-Null
         $updates = Get-WindowsUpdate
     } until ($updates.count -eq 0)
 }
-
 
 # Set Start Menu Layout to Empty
 Copy-Item './LayoutModification.xml' C:\Users\$env:UserName\AppData\Local\Microsoft\Windows\Shell -Force
@@ -51,13 +61,13 @@ Set-ItemProperty $key 'SearchboxTaskbarMode' 0     # Hide search box on taskbar
 Stop-Process -processname explorer
 
 # Setup Chocolatey package manager 
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install Apps using Chocolatey
 # GUI Package Manager
 choco install chocolateygui -y
 # Utility Apps
-choco install aida64-extreme
+choco install aida64-extreme -y
 # MS Office Apps
 choco install office365business -y -params '"/productid:O365HomePremRetail /exclude:""Access OneNote Publisher""'
 choco pin office365business -y
@@ -65,49 +75,51 @@ choco pin office365business -y
 choco install adobe-creative-cloud -y
 choco pin adobe-creative-cloud -y
 
-# Dowload and install MS Store App Sideloader
-$url = 'https://github.com/microsoft/winget-cli/releases/download/v0.2.2941/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle'
-$path = './Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle'
-Invoke-WebRequest -Uri $url -OutFile $path
-Add-AppxPackage -Path $path
+if ($updateChoice -eq 'y') {
+    # Dowload and install MS Store App Sideloader
+    $url = 'https://github.com/microsoft/winget-cli/releases/download/v0.2.2941/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle'
+    $path = './Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle'
+    Invoke-WebRequest -Uri $url -OutFile $path
+    Add-AppxPackage -Path $path
 
-# Install PowerShell 7
-winget install 'PowerShell'
-reg import .\reg-files\Add_64-bit_PowerShell_7_Open_here_context_menu_on_64-bit_Windows_10.reg
+    # Install PowerShell 7
+    winget install 'PowerShell'
+    reg import .\reg-files\Add_64-bit_PowerShell_7_Open_here_context_menu_on_64-bit_Windows_10.reg
 
-# Install Other Desktop Apps
-winget install 'Logitech Gaming Hub'
-winget install 'PowerToys'
-winget install 'Visual Studio Code'
-winget install 'Visual Studio Community'
-winget install 'NordVPN'
-winget install 'Notepad++'
-winget install 'Nvidia GeForce Experience'
-winget install 'Plex for Windows'
-winget install 'Python'
-winget install 'Spotify'
-winget install 'Steam'
-winget install 'Google Chrome'
-winget install 'Firefox'
-winget install 'VLC media player'
-winget install 'Teamviewer'
-winget install 'Discord'
-winget install 'Dropbox'
-winget install 'TreeSize Free'
-winget install 'Link Shell Extension'
-winget install 'Audacity'
-winget install 'PuTTY'
-winget install 'CPU-Z'
-winget install '7Zip'
-winget install 'WinSCP'
-winget install 'GitHub Desktop'
-winget install 'Ubisoft Connect'
-winget install 'Teamspeak Client'
+    # Install Other Desktop Apps
+    winget install 'Logitech Gaming Hub'
+    winget install 'PowerToys'
+    winget install 'Visual Studio Code'
+    winget install 'Visual Studio Community'
+    winget install 'NordVPN'
+    winget install 'Notepad++'
+    winget install 'Nvidia GeForce Experience'
+    winget install 'Plex for Windows'
+    winget install 'Python'
+    winget install 'Spotify'
+    winget install 'Steam'
+    winget install 'Google Chrome'
+    winget install 'Firefox'
+    winget install 'VLC media player'
+    winget install 'Teamviewer'
+    winget install 'Discord'
+    winget install 'Dropbox'
+    winget install 'TreeSize Free'
+    winget install 'Link Shell Extension'
+    winget install 'Audacity'
+    winget install 'PuTTY'
+    winget install 'CPU-Z'
+    winget install '7Zip'
+    winget install 'WinSCP'
+    winget install 'GitHub Desktop'
+    winget install 'Ubisoft Connect'
+    winget install 'Teamspeak Client'
 
-# Install MS Store Apps
-winget install 'Windows Terminal'
-winget install 'Ubuntu'
-winget install 'Debian'
+    # Install MS Store Apps
+    winget install 'Windows Terminal'
+    winget install 'Ubuntu'
+    winget install 'Debian'
+}
 
 # Delete Desktop Shortcuts
 Remove-Item C:\Users\$env:UserName\Desktop\*.lnk -Force
